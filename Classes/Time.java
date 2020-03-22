8d764fe187d29aec6e4c18b79f280b37b669e940
Albedaiwi <60201228+Naz2020@users.noreply.github.com>
2020-03-22 15:37:31 -0700

    Update Time.java

--- Classes/Time.java
+++ Classes/Time.java
@@ -1,1 +1,29 @@
 
+Anas
+
+public class Times 
+{ 
+    public static void main (String[] args) throws InterruptedException 
+    {
+        boolean timeT = true;
+        long showMim = 0;
+        long timebegin = currentTimeMillis();
+        System.out.println("Time runing : ");
+       
+        while( timeT )
+        {
+            TimeUnit.SECONDS.sleep(1);
+            long timeGone  = currentTimeMillis() - timebegin ;
+            long secTime = timeGone/1000;
+            
+            if( secTime == 60) 
+            {
+                secTime = 0;
+                timebegin = currentTimeMillis();
+            }
+            if( secTime == 60 )
+            showMim ++;
+            System.out.println(showMim+":"+secTime);
+        }
+    }
+}
