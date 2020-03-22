1c7413734812fb3e26027248ceb7c3d1b1ab4a61
Albedaiwi <60201228+Naz2020@users.noreply.github.com>
2020-03-22 16:35:00 -0700

    Update Time.java

--- Classes/Time.java
+++ Classes/Time.java
@@ -1,9 +1,7 @@
 
-Anas
-
 public class Times 
 { 
-    public static void main (String[] args) throws InterruptedException 
+    public static void main (String[] args) 
     {
         boolean timeT = true;
         long showMim = 0;
@@ -12,18 +10,22 @@ public class Times
        
         while( timeT )
         {
-            TimeUnit.SECONDS.sleep(1);
-            long timeGone  = currentTimeMillis() - timebegin ;
-            long secTime = timeGone/1000;
-            
-            if( secTime == 60) 
-            {
-                secTime = 0;
-                timebegin = currentTimeMillis();
+            try {
+                TimeUnit.SECONDS.sleep(1);
+                long timeGone  = currentTimeMillis() - timebegin ;
+                long secTime = timeGone/1000;
+                
+                if( secTime == 60)
+                {
+                    secTime = 0;
+                    timebegin = currentTimeMillis();
+                }
+                if( secTime == 60 )
+                    showMim++;
+                System.out.println(showMim+":"+secTime);
+            } catch (InterruptedException ex) {
+                Logger.getLogger(Times.class.getName()).log(Level.SEVERE, null, ex);
             }
-            if( secTime == 60 )
-            showMim ++;
-            System.out.println(showMim+":"+secTime);
         }
     }
 }
