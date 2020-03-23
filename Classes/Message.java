1fbfe8763b9e9efc0b2e3fd9a0f199551e7f39de
Cellexs <60493195+Cellexs@users.noreply.github.com>
2020-03-22 21:56:19 -0700

    Update Message.java

--- Classes/Message.java
+++ Classes/Message.java
@@ -1,1 +1,24 @@
+package BVOC_package;
 
+public class Message
+{
+	// The name of the person that sent the message
+	Profile sender;
+	
+	// The text that was sent
+	String text;
+
+	// Constructor that creates a message object
+	public Message(Profile profile, String message)
+	{
+		sender = profile;
+			
+		text = message;
+	}
+	
+	// Converts the message object to a string format
+	public String toString()
+	{
+		return (sender.profileName + " says: " + text);
+	}
+}
