ac7bfcaf4a7dfb502c1dc22b7d8745d9f7ed426a
Cellexs <60493195+Cellexs@users.noreply.github.com>
2020-03-22 21:57:05 -0700

    Update History.java

--- Classes/History.java
+++ Classes/History.java
@@ -1,1 +1,54 @@
+package BVOC_package;
 
+public class History 
+{
+	final int DEFAULT = 10;
+	
+	// Creates the array that holds all message history
+	Message[] messages;
+	
+	// Tracks the amount of messages the account has
+	int messagesSize;
+	int messagesCap;
+	
+	// Default constructor for a History object
+	public History()
+	{
+		messages = new Message[DEFAULT];
+		messagesSize = 0;
+		messagesCap = DEFAULT;
+	}
+	
+	public void addMessage(Message newMessage)
+	{
+		if (messagesSize > messagesCap)
+		{
+			resize();
+		}
+		
+		messages[messagesSize] = newMessage;
+		
+		messagesSize++;
+	}
+	
+	public void resize()
+	{
+		int newCap;
+		int arrDex;
+		
+		// Resizes followed if needed
+		if (messagesSize > messagesCap)
+		{
+			newCap = messagesCap * 2;
+			
+			Message[] tempArray = new Message[newCap];
+			
+			for(arrDex = 0; arrDex < messagesSize; arrDex++)
+			{
+				tempArray[arrDex] = messages[arrDex];
+			}
+			
+			messages = tempArray;
+		}	
+	}
+}
