b4af4f66047bc219142b0993a485f2e0f1ba899d
Cellexs <60493195+Cellexs@users.noreply.github.com>
2020-03-22 23:21:59 -0700

    Update Database.java

--- Classes/Database.java
+++ Classes/Database.java
@@ -13,7 +13,8 @@ import javax.servlet.http.HttpServletResponse;
  */
 public class Database extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
+    Profile[] database;
+	
     /**
      * @see HttpServlet#HttpServlet()
      */
@@ -22,7 +23,7 @@ public class Database extends HttpServlet {
         super();
         // TODO Auto-generated constructor stub
         
-        Profile[] database = new Profile[30];
+        database = new Profile[30];
     }
 
 	/**
@@ -42,7 +43,94 @@ public class Database extends HttpServlet {
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		doGet(request, response);
+		
+		if(request.FormValue(problem) == "profile")
+		{
+			
+		}
+		else
+		{
+			Profile current;
+			int arrDex = 0;
+			boolean loopFlag = true;
+			
+			while( loopFlag )
+			{
+				if (request.FormValue(key) == database[arrDex].profilePassword)
+				{
+					current = database[arrDex];
+					loopFlag = false;
+				}
+				
+				arrDex++;
+			}
+			
+			// Handles the adding of fields
+			if(request.FormValue(problem) == "field")
+			{
+				current.fields[current.fieldSize] = Field(request.FormValue(fieldName), request.FormValue(fieldData));
+				response = "You successfully added a field!";
+			}
+			
+			// Handles the sending of messages
+			if(request.FormValue(problem) == "message")
+			{
+				Message newMessage = Message(current, request.FormValue(messageData));
+				
+				Profile reciever;
+				arrDex = 0;
+				loopFlag = true;
+				
+				while( loopFlag )
+				{
+					if (request.FormValue(recipient) == database[arrDex].profileName)
+					{
+						reciever = database[arrDex];
+						loopFlag = false;
+					}
+					
+					arrDex++;
+				}
+				
+				reciever.profileHistory.addMessage(newMessage);
+				response = "You successfully messaged " + reciever.profileName + "!";
+			}
+			
+			// Handles for adding a follower to a profile
+			if(request.FormValue(problem) == "follow")
+			{
+				Profile desiredFollow;
+				arrDex = 0;
+				loopFlag = true;
+				
+				while( loopFlag )
+				{
+					if (request.FormValue(recipient) == database[arrDex].profileName)
+					{
+						desiredFollow = database[arrDex];
+						loopFlag = false;
+					}
+					
+					arrDex++;
+				}
+				
+				desiredFollow.followed[desiredFollow.followedSize].addFollower(current);
+				response = "You successfully followed " + desiredFollow.profileName + "!";
+			}
+			
+			// Handles the displaying of history
+			if(request.FormValue(problem) == "history")
+			{
+				String tempRes = current.profileHistory.messages[0].toString();
+				
+				for(arrDex = 1; arrDex < current.profileHistory.messagesSize; arrDex++)
+				{
+					tempRes += "," + current.profileHistory.messages[arrDex].toString();
+				}
+				
+				response = tempRes;
+			}
+		}
 	}
-
 }
+
