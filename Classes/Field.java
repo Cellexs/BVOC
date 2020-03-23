7656317509d1e3aba3f04abf8c8630955e8c1bb5
Cellexs <60493195+Cellexs@users.noreply.github.com>
2020-03-22 21:56:39 -0700

    Update Field.java

--- Classes/Field.java
+++ Classes/Field.java
@@ -1,1 +1,24 @@
+package BVOC_package;
 
+public class Field 
+{
+	// User entered name for the field
+	String name;
+	
+	// User entered data about the field
+	String data;
+	
+	// Constructor for field objects
+	public Field(String fieldName, String text)
+	{
+		name = fieldName;
+		
+		data = text;
+	}
+	
+	// Converts a fields information to a string format
+	public String toString()
+	{
+		return (name + " has been updated to " + data);
+	}
+}
