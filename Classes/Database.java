99d4b09eb707eabb5b340a790b66819aee25a159
Cellexs <60493195+Cellexs@users.noreply.github.com>
2020-03-22 21:57:55 -0700

    Create Database.java

---
+++ Classes/Database.java
@@ -0,0 +1,48 @@
+package BVOC_package;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class Database
+ */
+public class Database extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public Database() 
+    {
+        super();
+        // TODO Auto-generated constructor stub
+        
+        Profile[] database = new Profile[30];
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+		
+		PrintWriter out = response.getWriter();
+		
+		out.println(" Hello World!");
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
