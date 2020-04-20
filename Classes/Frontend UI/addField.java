788681da22ddd9b2f1607efc7232b293a748671e
azizalfort <60493351+azizalfort@users.noreply.github.com>
2020-04-19 21:55:43 -0700

    Update addField.java

--- Classes/Frontend UI/addField.java
+++ Classes/Frontend UI/addField.java
@@ -1,1 +1,15 @@
+package com.cs386.bvoc;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class addField extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_field);
+    }
+}
 
