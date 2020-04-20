9a522c73927db243aa4261628b51983d45e8a2e2
azizalfort <60493351+azizalfort@users.noreply.github.com>
2020-04-19 21:56:24 -0700

    Update History.java

--- Classes/Frontend UI/History.java
+++ Classes/Frontend UI/History.java
@@ -1,1 +1,16 @@
+package com.cs386.bvoc;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class History extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_history);
+    }
+
+}
 
