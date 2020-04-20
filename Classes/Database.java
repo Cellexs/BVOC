26633ae191de4bbfc7767a068a2f4ac247057655
Cellexs <60493195+Cellexs@users.noreply.github.com>
2020-04-19 21:23:26 -0700

    Update Database.java

--- Classes/Database.java
+++ Classes/Database.java
@@ -11,9 +11,13 @@ import javax.servlet.http.HttpServletResponse;
 /**
  * Servlet implementation class Database
  */
-public class Database extends HttpServlet {
+	public class Database extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-    Profile[] database;
+    
+	int dataSize = 0;
+    final int DATA_CAP = 30;
+	
+	Profile[] database;
 	
     /**
      * @see HttpServlet#HttpServlet()
@@ -23,114 +27,149 @@ public class Database extends HttpServlet {
         super();
         // TODO Auto-generated constructor stub
         
-        database = new Profile[30];
+        database = new Profile[DATA_CAP];     
+    }
+    
+    public boolean addProfile(Profile newProf)
+    {
+    	if (dataSize > DATA_CAP)
+    	{
+    		return false;
+    	}
+    	
+    	database[dataSize] = newProf;
+    	
+    	return true;
     }
 
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException 
+	{
 		// TODO Auto-generated method stub
 		response.getWriter().append("Served at: ").append(request.getContextPath());
 		
 		PrintWriter out = response.getWriter();
 		
-		out.println(" Hello World!");
+		out.println(" Hello World! ");
 	}
 
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException 
+	{
 		// TODO Auto-generated method stub
+		PrintWriter out = response.getWriter();
+		boolean success;
 		
-		if(request.FormValue(problem) == "profile")
+		if(request.getParameter("problem") == "profile")
 		{
+			// Makes a new profile object and adds it to the database
+			Profile newProfile = new Profile(request.getParameter("profName"), request.getParameter("profID"));
 			
+			success = addProfile(newProfile);
+			
+			if(success)
+			{
+				out.println("Profile Created!");
+				return;
+			}
+			else
+			{
+				out.println("Profile Creation Failed!");
+				return;
+			}
 		}
-		else
-		{
-			Profile current;
-			int arrDex = 0;
-			boolean loopFlag = true;
 			
+		Profile current = null;
+		int arrDex = 0;
+		boolean loopFlag = true;
+			
+		while( loopFlag )
+		{
+			if (request.getParameter("profID") == database[arrDex].profileID)
+			{
+				current = database[arrDex];
+				loopFlag = false;
+			}
+				
+			arrDex++;
+		}	
+			
+		// Handles the adding of fields
+		if(request.getParameter("problem") == "field")
+		{
+			Field newField = new Field(request.getParameter("fieldName"), request.getParameter("fieldData"));
+			current.addToObject(current.fields, current.fieldSize, current.fieldCap, newField);
+			out.println("Field Has Been Added!");
+			return;
+		}
+			
+		// Handles the sending of messages
+		if(request.getParameter("problem") == "message")
+		{
+			Message newMessage = new Message(current, request.getParameter("messageData"));
+			
+			Profile receiver = null;
+			arrDex = 0;
+			loopFlag = true;
+				
 			while( loopFlag )
 			{
-				if (request.FormValue(key) == database[arrDex].profilePassword)
+				if (request.getParameter("recipient") == database[arrDex].profileName)
 				{
-					current = database[arrDex];
+					receiver = database[arrDex];
 					loopFlag = false;
 				}
-				
+					
 				arrDex++;
 			}
-			
-			// Handles the adding of fields
-			if(request.FormValue(problem) == "field")
-			{
-				current.fields[current.fieldSize] = Field(request.FormValue(fieldName), request.FormValue(fieldData));
-				response = "You successfully added a field!";
-			}
-			
-			// Handles the sending of messages
-			if(request.FormValue(problem) == "message")
-			{
-				Message newMessage = Message(current, request.FormValue(messageData));
 				
-				Profile reciever;
-				arrDex = 0;
-				loopFlag = true;
+			receiver.profileHistory.addMessage(newMessage);
+			
+			out.println("Message Sent!");
+			return;
+		}
+			
+		// Handles for adding a follower to a profile
+		if(request.getParameter("problem") == "follow")
+		{
+			Profile follower = null;
+			arrDex = 0;
+			loopFlag = true;
 				
-				while( loopFlag )
+			while( loopFlag )
+			{
+				if (request.getParameter("recipient") == database[arrDex].profileName)
 				{
-					if (request.FormValue(recipient) == database[arrDex].profileName)
-					{
-						reciever = database[arrDex];
-						loopFlag = false;
-					}
+					follower = database[arrDex];
+					loopFlag = false;
+				}
 					
-					arrDex++;
-				}
-				
-				reciever.profileHistory.addMessage(newMessage);
-				response = "You successfully messaged " + reciever.profileName + "!";
+				arrDex++;
 			}
+				
+			follower.addToObject(follower.followed, follower.followedSize, follower.followedCap, current);
+			out.println("You are now following " + follower.profileName + "!");
+			return;
+		}
 			
-			// Handles for adding a follower to a profile
-			if(request.FormValue(problem) == "follow")
+		// Handles the displaying of history
+		if(request.getParameter("problem") == "history")
+		{
+			String tempRes = current.profileHistory.messages[0].toString();
+				
+			for(arrDex = 1; arrDex < current.profileHistory.messagesSize; arrDex++)
 			{
-				Profile desiredFollow;
-				arrDex = 0;
-				loopFlag = true;
-				
-				while( loopFlag )
-				{
-					if (request.FormValue(recipient) == database[arrDex].profileName)
-					{
-						desiredFollow = database[arrDex];
-						loopFlag = false;
-					}
-					
-					arrDex++;
-				}
-				
-				desiredFollow.followed[desiredFollow.followedSize].addFollower(current);
-				response = "You successfully followed " + desiredFollow.profileName + "!";
+				tempRes += "," + current.profileHistory.messages[arrDex].toString() + "\n";
 			}
-			
-			// Handles the displaying of history
-			if(request.FormValue(problem) == "history")
-			{
-				String tempRes = current.profileHistory.messages[0].toString();
 				
-				for(arrDex = 1; arrDex < current.profileHistory.messagesSize; arrDex++)
-				{
-					tempRes += "," + current.profileHistory.messages[arrDex].toString();
-				}
-				
-				response = tempRes;
-			}
+			out.println(tempRes);
+			return;
 		}
 	}
 }
+	
 
