b50a42ecd53724a92b9db8b7c1c1c60baa5e28cb
Cellexs <60493195+Cellexs@users.noreply.github.com>
2020-04-19 22:07:29 -0700

    Update GroupMessage.java

--- Classes/GroupMessage.java
+++ Classes/GroupMessage.java
@@ -1,128 +1,37 @@
+package BVOC_package;
 
-package groupchat;
-/**
- *
- * @author albedaiwi
- */
-
-public class GroupChat
+public class GroupMessage extends Message
 {
-    private String acount;
-    public boolean chat;
-    private int port;
-    private int size = 10;
-    public static int Id; 
-    /**
-     * 
-     * @param acount 
-     */
-     GroupChat(String acount)
-    {
-        this.acount = acount; 
-    }
-     /**
-      * 
-      * @return 
-      */
-     String getAcount()
-     {
-         return acount;
-     }
-     /**
-      * 
-      * @param userAcount 
-      */
-    public static void addUsers(String userAcount)
-    {
-        acount.//something(userAcount);
-        updateuserAcount();
-    }
-    /**
-     * 
-     * @param userAcount 
-     */
-    public static void removeUser(String userAcount)
-    {
-        int index;
-        for(index = 0; index < userAcount.size(); index++)
-        {
-            if(userAcount.get(index).equalsIgnoreCase(userAcount))
-                userAcount.removeuser(index);
-        }
-    
-        updateuserAcount();
-    }
-    /**
-     * 
-     * @return 
-     */
-    public static int getNumberOfUsers()
-    {
-        return acount.size();
-    }
-    /**
-     * 
-     */
-    public static void updateuserAcount()
-    {
-       
-    }
- 
-    /**
-     * 
-     * @param port 
-     */
-    public GroupChat(int port)
-    {
-        this.port = port;
-    }
-    /**
-     * 
-     */
-    public void startChat()
-    {
-        boolean chat = true;
-    {
-        GroupChat serverPort = new GroupChat(port);
-    }
-        while (chat)
-        {
-           
-           String text = null;
-           String display = text;
-           text = (" server is waiting for the port "+port);
-            
-        //   GroupChat portSer = GroupChat.something();
-           if(!chat)
-               break;
-        }
-    }
-    /**
-     * 
-     */
-    public void stopChat()
-    {
-        chat = false;
-    }
-    /**
-     * 
-     * @param text 
-     */
-    public void display(String text)
-    {
-        System.out.println(text);
-        
-    }
-    public static void main(String[] args)
-{
-    int serverNum = 2222;   
-    GroupChat port = new GroupChat(serverNum);
-    port.startChat();
-        
-}
-    public class textMessage extends Message()
-    {
-       return Message();
-    }
-    
+	final int DEFAULT_CAP = 5;
+	
+	int recipSize = 0;
+	
+	Profile sender;
+	
+	String text;
+	
+	Profile[] recipients = new Profile[DEFAULT_CAP];
+	
+	// Creates the object and saves the sender, text and recipients
+	public GroupMessage(Profile profile, String message, Profile[] receivers)
+	{
+		sender = profile;
+			
+		text = message;
+		
+		recipients = receivers;
+		
+		sendGroupMessage();
+	}
+	
+	// Adds the object to the history of all profiles
+	public void sendGroupMessage()
+	{
+		int arrDex;
+		
+		for(arrDex = 0; arrDex < recipSize; arrDex++)
+		{
+			recipients[arrDex].profileHistory.addMessage(this);
+		}
+	}
 }
