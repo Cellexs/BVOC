b9675b093a3b57bb7feb2faf9a84876592c65582
azizalfort <60493351+azizalfort@users.noreply.github.com>
2020-04-19 21:57:37 -0700

    Update Message.java

--- Classes/Frontend UI/Message.java
+++ Classes/Frontend UI/Message.java
@@ -1,1 +1,14 @@
+package com.cs386.bvoc;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Message extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_message);
+    }
+}
 
