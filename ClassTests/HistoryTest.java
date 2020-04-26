59b7364de2b2b6553a0adfb4770a909f7dbc1784
azizalfort <60493351+azizalfort@users.noreply.github.com>
2020-04-26 16:10:21 -0700

    Update HistoryTest.java

--- ClassTests/HistoryTest.java
+++ ClassTests/HistoryTest.java
@@ -1,1 +1,42 @@
 
+package bvoc.test;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+
+public class HistoryTest {
+    
+    public HistoryTest() 
+    {
+    }
+
+    /**
+     * Test of addMessage method, of class History.
+     */
+    @Test
+    public void testAddMessage() 
+    {
+        String name = "John";
+        String id = "12345";
+        String message = "Hello World";
+        Profile profile = new Profile(name, id);
+        Message newMessage = new Message(profile, message);
+        
+        History instance = new History();
+        instance.addMessage(newMessage);
+      
+    }
+
+    /**
+     * Test of resize method, of class History.
+     */
+    @Test
+    public void testResize() {
+        
+        History instance = new History();
+        instance.resize();  
+    }
+    
+}
+
